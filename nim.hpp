de4504047e0b57608937880ab6ddfcde09c0a561
A Crutcher <alcrutcher14@gmail.com>
2016-02-15 10:24:41 -0600

    Dates
    I forgot to update the last modified dates and change the header from
    nim.h to nim.hpp

--- nim.hpp
+++ nim.hpp
@@ -1,10 +1,10 @@
-/* nim.h
+/* nim.hpp
 Author:		Andrew Crutcher
 Email:		alcrutcher1s@semo.edu
 College:	Southeast Missouri State University
 Course:		MA138-01 Discrete Mathematics I
 Prof:		Dr. Daly
-Date:		11/27/2015
+Date:		02/15/2016
 Description: A header containing the game of Nim functions.
 */
 
